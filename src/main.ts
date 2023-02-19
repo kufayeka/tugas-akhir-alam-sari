1fffcce7064b6e595a01b35f621db0372a935755
Yeka Kufa <yayankufa18@gmail.com>
2023-02-19 14:54:45 +0700

    optimizing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp, onBeforeMount, onMounted } from "vue";
 import { createPinia } from "pinia";
-const LottieAnimation = () => import("lottie-web-vue");
 import MainRouter from "@/router/index";
 import "./assets/tailwind.css";
 import Application from "@/App.vue";
@@ -9,41 +8,54 @@ import Application from "@/App.vue";
 import bootstrapAllModules from "@/modules-bootstrapper";
 
 // Import Capacitor Plugins
-import { Capacitor } from "@capacitor/core";
 import { App } from "@capacitor/app";
 import { SplashScreen } from "@capacitor/splash-screen";
 
+// Initialize the Vue.js application
 const initApp = async () => {
+  // Create a new instance of Pinia store
   const pinia = createPinia();
+
+  // Create a new instance of Vue router
   const router = MainRouter;
 
+  // Create a new instance of the Vue app
   const app = createApp(Application);
 
+  // Use all modules bootstrapper to add global Vue plugins and components
   bootstrapAllModules.forEach((x) => app.use(x));
 
-  app.use(router).use(pinia).use(LottieAnimation);
+  // Add the router and Pinia store to the Vue app
+  app.use(router).use(pinia);
 
+  // Mount the Vue app to the DOM element with ID "app"
   app.mount("#app");
 
-  // report error to tracking services
+  // Set up an error handler to report any errors to tracking services
   app.config.errorHandler = (err, instance, info) => {
     console.log("REPORT:", err, instance, info);
   };
 };
 
+// Initialize the Capacitor plugins on app start up
 const runCapacitorPluginOnInit = async () => {
+  // Show the splash screen while the app loads
   await SplashScreen.show();
 
+  // Add a listener to detect when the app state changes
   await App.addListener("appStateChange", (state) => {
-    console.log("WEB SECTION: APP is active");
+    console.log("App is active");
     if (state.isActive) {
-      console.log("WEB SECTION: APP is active");
+      console.log("App is active");
     }
   });
 };
 
+// Run the Capacitor plugin initialization function
 runCapacitorPluginOnInit();
 
+// Run the Vue app initialization function
 initApp();
 
+// Export an empty object to avoid TypeScript warnings
 export {};
